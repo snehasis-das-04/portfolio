5895c768b70ce1ac2807175e49f9d61159b22217
snehasis-das-04 <snehasisdas@2005@gmail.com>
2025-10-24 21:37:56 +0530

    push

--- js/app.js
+++ js/app.js
@@ -1,8 +1,20 @@
-$(document).ready(function(){
-    $('.slider').slick({
-       arrows:false,
-       dots:true,
-       appendDots:'.slider-dots',
-       dotsClass:'dots'
+$(document).ready(function () {
+	$('.slider').slick({
+		arrows: false,
+		dots: true,
+		appendDots: '.slider-dots',
+		dotsClass: 'dots',
+	});
+
+
+	let hamburger = document.querySelector('.hameburger');
+	let closee = document.querySelector('.closee');
+	let mobileNav = document.querySelector('.mobile-nav');
+
+	hamburger.addEventListener('click', function () {
+		mobileNav.classList.add('open');
+	});
+	closee.addEventListener('click', function () {
+       mobileNav.classList.remove('open');  
     });
-})
+});
